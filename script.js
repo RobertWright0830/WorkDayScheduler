29bd77855185f9bed495a42e9f5fd39b7067d76e
RobertWright0830 <dnafrankenstein@gmail.com>
2023-09-23 07:38:06 -0500

    added date header to html and js; added plugin advanced format for ordinal numbers; added date header logic to pull from dayjs; created loop to dynamically add hours; determine whether hours are past, present, future; add related attributes; distinguish between AM and PM hours; add additional elements for each hour

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+$(document).ready(function () {
+
+  var currentHour = dayjs().hour();
+
+  var pastContainer = document.getElementById("past-container");
+  var presentContainer = document.getElementById("present-container");
+  var futureContainer = document.getElementById("future-container");
+
+  for (var hour = 9; hour <= 17; hour++) {
+    var block = document.createElement("div");
+    block.className = "row time-block";
+    block.setAttribute("data-hour", hour);
+   
+    if (hour < currentHour) {
+      pastContainer.appendChild(block);
+    } else if (hour === currentHour) {
+      presentContainer.appendChild(block);
+    } else {
+      futureContainer.appendChild(block);
+    }
+
+    if (hour < currentHour) {
+      block.classList.add("past");
+    } else if (hour === currentHour) {
+      block.classList.add("present");
+    } else {
+      block.classList.add("future");
+    }
+    var hourText = document.createElement("div");
+hourText.className = "col-2 col-md-1 hour text-center py-3";
+
+if (hour === 12) {
+  hourText.textContent = "12PM";
+} else if (hour > 12) {
+  hourText.textContent = (hour - 12) + "PM";
+} else {
+  hourText.textContent = hour + "AM";
+}
+
+    var textarea = document.createElement("textarea");
+    textarea.className = "col-8 col-md-10 description";
+    textarea.rows = 3;
+
+    var saveButton = document.createElement("button");
+    saveButton.className = "btn saveBtn col-2 col-md-1";
+    saveButton.setAttribute("aria-label", "save");
+
+    var saveIcon = document.createElement("i");
+    saveIcon.className = "fas fa-save";
+    saveIcon.setAttribute("aria-hidden", "true");
+
+    saveButton.appendChild(saveIcon);
+    
+    block.appendChild(hourText);
+    block.appendChild(textarea);
+    block.appendChild(saveButton);
+
+
+  }
+});
+
+
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,15 +68,13 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+    
+  // Displays the current date in the header of the page.
+  dayjs.extend(dayjs_plugin_advancedFormat);
+  var currentDate = dayjs().format('dddd, MMMM Do');
+  document.getElementById('currentDay').textContent = currentDate;
+
