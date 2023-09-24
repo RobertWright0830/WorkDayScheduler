6516b401ff14943b4bb574c8915e65da82eb3820
RobertWright0830 <dnafrankenstein@gmail.com>
2023-09-24 08:12:40 -0500

    added save event listener and local storage, adjusted local storage so blocks are refreshed when date changes, added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # WorkDayScheduler
+
+## Description
+
+This website allows a user to add important events to a daily planner so they can manage their time effectively.
+
+This was accomplished by:
+
+-setting up a daily planner to create a schedule for the day in hour blocks
+
+-when the planner is opened, the current day is displayed at the top of the calendar
+
+-when the user scrolls down, they are presented with time blocks for the standard business hours of 9AM throgh 5PM
+
+-each time block is color coded to distinguish between past, present and future blocks
+
+-when the user clicks in a block they can enter an event
+
+-when the user clicks on the save button for the time block, the text for the event is saved in local storage
+
+-when the page is refreshed, the saved events persist for that date
+
+-when the date changes, events are cleared
+
+
+## Usage
+
+The deployed website can be found in the following location:
+
+https://robertwright0830.github.io/WorkDayScheduler/
+
+
+![Home Page](assets/images/WorkDayScheduler1.jpg)
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
+
+// Main function
 $(document).ready(function () {
+  // Get the current hour
   var currentHour = dayjs().hour();
 
+  // Store the current date in local storage
+  var currentDateInLocalStorage = localStorage.getItem("currentDate");
+
+  //If not in local storage, set it and clear all data
+  //if stored date is different from current date clear
+  //local storage and set the new current date
+  if (!currentDateInLocalStorage) {
+    localStorage.setItem("currentDate", getCurrentDate());
+    clearLocalStorageData();
+  } else {
+    if (currentDateInLocalStorage !== getCurrentDate()) {
+      clearLocalStorageData();
+      localStorage.setItem("currentDate", getCurrentDate());
+    }
+  }
+  //Get handle for past, present and future containers
   var $pastContainer = $("#past-container");
   var $presentContainer = $("#present-container");
   var $futureContainer = $("#future-container");
 
+  // Loop through hours (9AM to 5PM)
   for (var hour = 9; hour <= 17; hour++) {
+    // Create a time block element for each loop
     var $block = $("<div>").addClass("row time-block").attr("data-hour", hour);
 
+    // Determine the past present or future container for each time block
     if (hour < currentHour) {
       $pastContainer.append($block);
       $pastContainer.addClass("past");
@@ -19,8 +41,8 @@ $(document).ready(function () {
       $futureContainer.addClass("future");
     }
 
+    // Create and add elements for each time block
     var $hourText = $("<div>").addClass("col-2 col-md-1 hour text-center py-3");
-
     if (hour === 12) {
       $hourText.text("12PM");
     } else if (hour > 12) {
@@ -44,22 +66,42 @@ $(document).ready(function () {
     $block.append($hourText);
     $block.append($textarea);
     $block.append($saveButton);
+
+    // Event listeners for save buttons for each time block
+    $saveButton.on("click", saveTimeBlockFunction);
+
+    //Function to save the time block
+    function saveTimeBlockFunction() {
+      var hourId = $(this).parent(".time-block").data("hour");
+      var textareaContent = $(this).siblings(".description").val();
+      localStorage.setItem("hour-" + hourId, textareaContent);
+    }
+  }
+
+  // Loop through hours to get saved data from local storage
+  for (var hour = 9; hour <= 17; hour++) {
+    var savedText = localStorage.getItem("hour-" + hour);
+    if (savedText) {
+      $(".time-block[data-hour='" + hour + "']")
+        .find(".description")
+        .val(savedText);
+    }
   }
 });
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
 
 // Displays the current date in the header of the page.
 dayjs.extend(dayjs_plugin_advancedFormat);
 var currentDate = dayjs().format("dddd, MMMM Do");
 document.getElementById("currentDay").textContent = currentDate;
+
+// Function to get the current date
+function getCurrentDate() {
+  return dayjs().format("YYYY-MM-DD");
+}
+
+// Function to clear local storage data
+function clearLocalStorageData() {
+  for (var hour = 9; hour <= 17; hour++) {
+    localStorage.removeItem("hour-" + hour);
+  }
+}
